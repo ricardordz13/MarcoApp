480e29ad70b640dd9156e7e994eb4054f61728ae
ricardordz13 <ricardo.rdz.trevino@gmail.com>
2024-11-14 22:16:42 -0600

    Initial Commit

---
+++ Marco/MarcoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarcoApp.swift
+//  Marco
+//
+//  Created by Ricardo Jorge Rodriguez Trevino on 14/11/24.
+//
+
+import SwiftUI
+
+@main
+struct MarcoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
